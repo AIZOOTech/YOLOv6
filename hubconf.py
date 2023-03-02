2badedea51ef7544eac9ac613bfa58aa5714b4a5
AIZOO <60804068+AIZOOTech@users.noreply.github.com>
2023-03-02 14:49:41 +0800

    Update hubconf.py
    Fix bug in hubconf.

--- hubconf.py
+++ hubconf.py
@@ -172,3 +172,5 @@ def yolov6l(class_names=CLASS_NAMES, device=DEVICE, img_size=640, conf_thres=0.2
 def custom(ckpt_path, class_names, device=DEVICE, img_size=640, conf_thres=0.25, iou_thres=0.45, max_det=1000):
     return Detector(ckpt_path, class_names, device, img_size=img_size, conf_thres=conf_thres,
                     iou_thres=iou_thres, max_det=max_det)
+  
+ # pr tutorial.
